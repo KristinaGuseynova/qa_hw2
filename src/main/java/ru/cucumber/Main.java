a5ca61f20c391e3cd04f5d7e6b877175709fd1c7
Kristina Guseynova <bittersweet2010@list.ru>
2024-09-10 14:43:12 +0400

    added getMessage method

--- src/main/java/ru/cucumber/Main.java
+++ src/main/java/ru/cucumber/Main.java
@@ -2,6 +2,10 @@ package ru.cucumber;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello new world!");
+        System.out.println(getMessage());
+    }
+
+    private static String getMessage() {
+        return "Hello new world!";
     }
 }
